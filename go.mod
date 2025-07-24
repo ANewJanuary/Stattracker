1e0927c5b39b12602b022c5c6435468b2549239e
artin <artin@tumbleweed.localdomain>
2025-07-24 11:45:35 +0200

    setting up go based on tutorial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ANewJanuary/Stattracker
+
+go 1.24.5
---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+import "github.com/ANewJanuary/Stattracker"
+
+func main() {
+	fmt.Println(quote.Go())
+}
